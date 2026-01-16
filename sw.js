828d98e3effdf73ae917f540e030fbb7e395f04f
ced <cedduf@hotmail.fr>
2026-01-16 08:57:47 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+
 const CACHE_NAME = 'outil-duree-v1.0';
 const ASSETS = [
   './',
@@ -7,24 +8,34 @@ const ASSETS = [
   './logo-192.png',
   './logo-512.png'
 ];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
+
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
+
+// Offline-first
 self.addEventListener('fetch', (event) => {
   const req = event.request;
   if (req.method !== 'GET') return;
   event.respondWith(
-    caches.match(req).then(res => res || fetch(req).then(netRes => {
-      const copy = netRes.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(()=>{});
-      return netRes;
-    }).catch(() => caches.match('./index.html')))
+    caches.match(req).then(res =>
+      res || fetch(req).then(netRes => {
+        const copy = netRes.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(()=>{});
+        return netRes;
+      }).catch(() => caches.match('./index.html'))
+    )
   );
 });
